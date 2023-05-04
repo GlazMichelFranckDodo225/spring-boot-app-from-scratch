38f3ac5e9402842ecea0871a5630c4098d0e1737
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-04 08:13:53 +0200

    Addition of 5 courses in the method getAllCourses() and implementation of the Endpoint '/courses/1' in the CourseController

---
+++ src/main/java/com/dgmf/springbootappfromscratch/service/CourseService.java
@@ -0,0 +1,10 @@
+package com.dgmf.springbootappfromscratch.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dgmf.springbootappfromscratch.entity.Course;
+
+public class CourseService {
+	private List<Course> courses = new ArrayList<>();
+}
--- src/main/java/com/dgmf/springbootappfromscratch/web/CourseController.java
+++ src/main/java/com/dgmf/springbootappfromscratch/web/CourseController.java
@@ -1,19 +1,28 @@
 package com.dgmf.springbootappfromscratch.web;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dgmf.springbootappfromscratch.entity.Course;
 
 @RestController
 public class CourseController {
+
 	@GetMapping("/courses")
 	public List<Course> getAllCourses() {
-		return Arrays.asList(new Course(1, "Learn Microservices", "Glaz Dodo"));
+		return Arrays.asList(new Course(1, "Learn Microservices", "Glaz Dodo"),
+				new Course(2, "Learn Java", "Glaz Dodo"), new Course(3, "Learn Spring Boot", "Glaz Dodo"),
+				new Course(4, "Learn Spring Security", "Glaz Dodo"), new Course(5, "Learn Spring AOP", "Glaz Dodo"));
 	}
-	
+
+	@GetMapping("/courses/1")
+	public Course getCourseDetails() {
+		return new Course(1, "Learn Microservices", "Glaz Dodo"); 
+	}
 
 }
